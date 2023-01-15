6f9d8cbb66ac1cedae0f076cc470ef0d3ac6c281
Robert Lutrick <r.lutrick@icloud.com>
2023-01-15 09:38:07 -0600

    Comment update

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,3 +1,4 @@
+// Utilize React Hooks, they will always start with the word use
 import React, { useState } from "react";
 
 const TodoList = (props) => {
@@ -6,7 +7,7 @@ const TodoList = (props) => {
     const [items, setItems] = useState([])  // Items/Objects built from text
 
     // First attempt with using a prop.checked
-    // This is incorrect due to props being immutable
+    // Unable to make it work due to props being immutable
     // const [checked, setChecked] = useState(props.checked)
 
     const handleSubmit = (e) => {
@@ -15,15 +16,15 @@ const TodoList = (props) => {
         if (text.length === 0) {
             return;
         }
-        // Orignally setItems([...items], text)
-        // Updated to include completed for input checkbox tag
+        // Orignally: setItems([...items], text)
+        // Updated to object to include completed key for input checkbox tag
         const item = {
             textData: text,
             completed: false
         }
         // Push new item to list with previous ...unpacked items
         setItems([...items, item]);
-        // reset text to blank here and call it with in the input tag with value={text}
+        // Reset text to blank and call it with in the input tag using value={text}
         setText('');
     }
 
@@ -32,22 +33,28 @@ const TodoList = (props) => {
         const allItemsExcept = items.filter((item, i) => {
             return i !== delIndex;
         })
-        // Then setItems with all except the one "removed"
+        // Then setItems with all except the one removed/omitted
         setItems(allItemsExcept)
     }
 
     function handleCheck(targetID) {
+        // Loop over items using map
         const updateItems = items.map((item, i) => {
+            // Find match
             if (targetID === i) {
+                // ======== Version 1 ========
                 // Mutate object value directly (not best practice)
                 // item.completed = !item.completed
 
+                // ======== Version 2 ========
                 // Update object by ...unpacking and setting value
                 const updatedItem = { ...item, completed: !item.completed };
                 return updatedItem
             }
+            // Return non updated item
             return item
         })
+        // Set items to the new updated list state
         setItems(updateItems)
 
 
@@ -76,7 +83,7 @@ const TodoList = (props) => {
                     className="form-control my-2"
                     type="text"
                     onChange={(e) => setText(e.target.value)}
-                    // value: default to text. on submit reset value to ''
+                    // value: default to text state; onSubmit reset value to ''
                     value={text} />
                 <button className="btn btn-primary d-flex align-items-start" type="submit">Add</button>
             </form>
@@ -84,10 +91,12 @@ const TodoList = (props) => {
                 // Iterate over items using .map(array, index)
                 items.map((item, i) => (
                     // div must contain key for react to keep track of
-                    // this is provided with the .map(array, **INDEX**)
+                    // this is provided with the key using .map([], **INDEX**)
                     <div className="d-flex align-items-center justify-content-between my-2" key={i}>
                         <div>
-                            {/* input: checked requires onChange in order to update the mapped item directly */}
+                            {/* input: checked set to item state's value.
+                                checked also requires onChange in order to update the item state directly
+                                passing in the index value */}
                             <input checked={item.completed} type="checkbox" className="mx-2" onChange={(e) => handleCheck(i)} />
                             {/* label: turnary opperator for objs attribute rendering ?true or :false */}
                             <label className={item.completed ? "text-decoration-line-through" : ""}>{item.textData}</label>
