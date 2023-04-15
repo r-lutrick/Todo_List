d4a1b2d10da6b4a1ea2995e130288790fec608ea
r-lutrick <r.lutrick@icloud.com>
2023-04-15 11:26:00 -0500

    Updates to app

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -3,12 +3,8 @@ import React, { useState } from "react";
 
 const TodoList = () => {
     // Getters and Setters for state
-    const [text, setText] = useState("")    // input text tag
-    const [items, setItems] = useState([])  // Items/Objects built from text
-
-    // First attempt with using a prop.checked
-    // Unable to make it work due to props being immutable
-    // const [checked, setChecked] = useState(props.checked)
+    const [text, setText] = useState("")    // Input text tag
+    const [items, setItems] = useState([])  // List of Objects
 
     const handleSubmit = (e) => {
         e.preventDefault(); // **prevent refresh**
@@ -16,8 +12,7 @@ const TodoList = () => {
         if (text.length === 0) {
             return;
         }
-        // Orignally: setItems([...items], text)
-        // Updated to object to include completed key for input checkbox tag
+        // item must include completed attribute in order for updatedItems to work
         const item = {
             textData: text,
             completed: false
@@ -28,26 +23,21 @@ const TodoList = () => {
         setText('');
     }
 
-    const handleDelete = (delIndex) => {
+    const handleDelete = (deleteItem) => {
         // Filter all items execpt the one being deleted based on index
         const allItemsExcept = items.filter((item, i) => {
-            return i !== delIndex;
+            return i !== deleteItem;
         })
         // Then setItems with all except the one removed/omitted
         setItems(allItemsExcept)
     }
 
-    function handleCheck(targetID) {
-        // Loop over items using map
-        const updateItems = items.map((item, i) => {
+    function handleCheck(checkItem) {
+        // use .map to loop over items in list to return an updated list
+        const updatedItems = items.map((item, i) => {
             // Find match
-            if (targetID === i) {
-                // ======== Version 1 ========
-                // Mutate object value directly (not best practice)
-                // item.completed = !item.completed
-
-                // ======== Version 2 ========
-                // Update object by ...unpacking and setting value
+            if (checkItem === i) {
+                // Update object by ...unpacking and witching completed value
                 const updatedItem = { ...item, completed: !item.completed };
                 return updatedItem
             }
@@ -55,25 +45,8 @@ const TodoList = () => {
             return item
         })
         // Set items to the new updated list state
-        setItems(updateItems)
-
-
-
-        // First attempt to get target item to remove, update then add back with updated state
-        // obviously this is incorrect!
-        // const targetItem = items.filter((item) => item.id === targetID)
-        // const item = {
-        //     id: targetItem.id,
-        //     textData: targetItem.text,
-        //     completed: !targetItem.completed
-        // }
-        // setItems([...items, item])
-        // setChecked(!checked)
+        setItems(updatedItems)
     }
-
-
-
-
 
     return (
         <div className="container w-50 m-auto ">
@@ -88,12 +61,12 @@ const TodoList = () => {
                 <button className="btn btn-primary d-flex align-items-start" type="submit">Add</button>
             </form>
             {
-                // Iterate over items using .map(itterativeVariable, index)
+                // Iterate over items using .map(iterativeVariable, index)
                 items.map((item, i) => (
                     // div must contain key for react to keep track of
                     // this is provided with the key using .map(iV, **INDEX**)
-                    <div className="d-flex align-items-center justify-content-between my-2" key={i}>
-                        <div>
+                    <div className="d-flex align-items-end justify-content-between my-2" key={i}>
+                        <div className="d-flex">
                             {/* input: checked set to item.completed state's value.
                                 checked also requires onChange in order to update the item state directly
                                 passing in the index value */}
